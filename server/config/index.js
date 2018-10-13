2480e1641443a573c18512a165f9039ae8b99227
robertobock <robertobock@gmail.com>
2018-10-13 11:22:36 -0400

    fix in Procfile

--- server/config/index.js
+++ server/config/index.js
@@ -1,3 +1,3 @@
-export const secret = process.env.SECRET || 'Supercali Fragilistico Espialidoso!!'
+export const secret = process.env.SECRET || 'SupercaliFragilisticoEspialidoso!!'
 export const mongoUrl = process.env.MONGODB_URI || 'mongodb://localhost/platzi-overflow'
 export const port = process.env.PORT || 3000;
